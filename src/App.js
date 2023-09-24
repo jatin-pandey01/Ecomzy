cf5bd3720acfbf256f9bc08cad73bfaf6bae5589
jatin-pandey01 <=>
2023-09-24 22:17:31 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       </div>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="*" element={<Home/>}/>
         <Route path="/cart" element={<Cart/>}/>
       </Routes>
       
